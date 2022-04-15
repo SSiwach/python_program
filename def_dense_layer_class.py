b68aa91cb8915400071ae708b07b7ff32d0ab6d1
Surender Siwach <54826291+SSiwach@users.noreply.github.com>
2022-04-15 21:52:52 +0530

    Create def_dense_layer_class.py

---
+++ def_dense_layer_class.py
@@ -0,0 +1,21 @@
+import numpy as np
+import nnfs 
+from nnfs.datasets import spiral_data
+
+nnfs.init()
+
+class Layer_Dense:
+  def __init__(self,n_inputs,n_neurons):
+    self.weights = 0.01*np.random.randn(n_inputs, n_neurons) #generating random data for weights 
+    self.biases = np.zeros((1,n_neurons)) #Biases data
+
+  def forward(self, inputs): 
+    self.output = np.dot(inputs, self.weights) + self.biases  #inputs from last layer to the next layer
+
+X,y = spiral_data(samples = 100, classes = 3)
+
+dense1 = Layer_Dense(2,3)
+
+dense1.forward(X)
+
+print(dense1.output[:5])
