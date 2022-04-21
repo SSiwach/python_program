046f33a1694469a9f2e0ce9ce4cf1931d4bc39ae
Surender Siwach <54826291+SSiwach@users.noreply.github.com>
2022-04-21 23:15:16 +0530

    Create mat_dot.py

---
+++ mat_dot.py
@@ -0,0 +1,16 @@
+matrix1 = np.array([[0,1],[1,3]])
+
+print(matrix1)
+
+print(np.dot(matrix1,matrix1))   #matrix or dot product
+
+
+print(matrix1*matrix1)
+
+stuff = np.zeros(10,float)
+
+t = np.arange(4)
+
+stuff[3:7] = np.sqrt(t+1)
+
+stuff[3:7]
