c1bcb0087c5d770abed20f3dfe7fdc952397b4f1
Surender Siwach <54826291+SSiwach@users.noreply.github.com>
2022-05-10 23:53:07 +0530

    Create array.py

---
+++ array.py
@@ -0,0 +1,10 @@
+# Vectors 
+import numpy as np
+x = np.random.rand(10)
+y = x**2
+v1 = [x,y] # list of variables
+v2 = (-1,2) # tuples of numbers
+#v3 =(x1, x2, x3) #tuples ofd variable
+from math import exp
+v4 = [exp(-i*0.1) for i in range(150)]
+v4
